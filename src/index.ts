044d54490bcc5808ab49b45d07ec970f5a9ef665
Aliaksei Volkau <stakebf@gmail.com>
2022-05-29 23:46:25 +0300

    hm2: add entry point

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import userRouter from '@routes/user';
+import errorHandler from '@src/errorHandler/errorHandler';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use(errorHandler);
+
+app.listen(process.env.PORT ?? 3050);
