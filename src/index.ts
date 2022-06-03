cee55c471bb698cac34afc5cedbb8e9ec701f022
Aliaksei Volkau <stakebf@gmail.com>
2022-06-03 15:03:28 +0300

    hm2: change name for users router

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', userRouter);
+app.use('/users', userRouter);
 app.use(errorHandler);
 
 app.listen(process.env.PORT ?? 3050);
