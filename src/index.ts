bbb9211d83bda1a77dc4fa61c972e7bef3e34df7
Aliaksei Volkau <stakebf@gmail.com>
2022-06-22 00:12:29 +0300

    hm3: add data-source

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import 'dotenv/config';
+import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import userRouter from '@routes/user';
 import errorHandler from '@src/errorHandler/errorHandler';
+import { PostgresDataSource } from '@src/data-source';
+import logger from '@src/logger';
 
 const app = express();
+
+PostgresDataSource.initialize()
+  .then(() => {
+    console.log('PostgresDataSource is initialized');
+  })
+  .catch((error) => {
+    console.log(error);
+    logger.error('Something went wrong with PostgresDataSource initialization');
+  });
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.use('/api/users', userRouter);
 app.use(errorHandler);
 
-app.listen(process.env.PORT ?? 3050);
+app.listen(process.env.DEFAULT_SERVER_PORT ?? 3050);
