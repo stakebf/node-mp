a4b93c7c08cd8f42bec25df7440b774a0f8e4921
Aliaksei Volkau <stakebf@gmail.com>
2022-06-22 00:12:42 +0300

    hm3: add migration

---
+++ src/migrations/1655839053915-CreateUserTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import UserEntity from '../entities/User';
+
+export class CreateUserTable1655839053915 implements MigrationInterface {
+  name = 'CreateUserTable1655839053915';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "login" character varying NOT NULL, "password" character varying NOT NULL, "age" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "UQ_a62473490b3e4578fd683235c5e" UNIQUE ("login"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))');
+
+    await queryRunner.manager.insert(UserEntity, {
+      id: 'a81bc81b-dead-4e5d-abff-90865d1e13b1',
+      login: 'test',
+      password: 'test123',
+      age: 20
+    });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE "user"');
+  }
+}
