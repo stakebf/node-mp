3923d592d17a7854c5329f10b4d580bb096b8914
Aliaksei Volkau <stakebf@gmail.com>
2022-06-22 00:11:38 +0300

    hm3: add entity

---
+++ src/entities/User.ts
@@ -0,0 +1,47 @@
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
+import bcrypt from 'bcrypt';
+
+@Entity({ name: 'user' })
+class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+  @Column({
+    unique: true
+  })
+    login: string;
+
+  @Column({
+    select: false
+  })
+    password: string;
+
+  @Column()
+    age: number;
+
+  @CreateDateColumn()
+    createdAt: Date;
+
+  @UpdateDateColumn()
+    updatedAt: Date;
+
+  @DeleteDateColumn()
+    deletedAt: Date;
+
+  @BeforeInsert()
+  async setPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(password ?? this.password, salt);
+  }
+}
+
+export default UserEntity;
